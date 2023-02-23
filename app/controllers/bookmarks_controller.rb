ab0c407b4baea60f068678d3a5be775919982a82
canozben <canozben@windowslive.com>
2023-02-23 16:32:49 +0100

    half done no movies display and no css

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,42 @@
 class BookmarksController < ApplicationController
-  def new
+  before_action :set_list, only: [:new, :create]
+  before_action :set_bookmark, only: :destroy
+def index
+  @bookmark = Bookmark.all
+end
 
+  def new
+    @bookmark = Bookmark.new
   end
 
   def create
-
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to lists_path
   end
 
   def show
-
+    @bookmark = Movie.find(params[:id])
   end
 
-  def delete
+  def destroy
+    @bookmark.destroy
+    @list = List.find(params[:list_id])
+    redirect_to list_path(@list)
+  end
 
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/show'
-  get 'bookmarks/delete'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -9,5 +7,5 @@ Rails.application.routes.draw do
   resources :lists, only: [:new, :create, :show, :index ] do
     resources :bookmarks, only: [:new, :create]
   end
-  resources :bookmarks, only:[:destroy]
+  resources :bookmarks, only: :destroy
 end
