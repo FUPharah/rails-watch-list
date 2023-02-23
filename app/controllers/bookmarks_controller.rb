56fbbaf2ebeecb1f5563488712b29d7a529e5f32
canozben <canozben@windowslive.com>
2023-02-23 12:54:31 +0100

    controllers done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,17 @@
+class BookmarksController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def delete
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/show'
+  get 'bookmarks/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:new, :create, :show, :index ] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only:[:destroy]
 end
